48e45ac9cdaca7e7b4328efdfd59df96c351912e
Rahul Sharma <raulshma@gmail.com>
2020-11-28 00:23:14 +0530

    Added splash screen to android 😃 (#1)

--- android/app/src/main/java/com/covid19tracker/MainActivity.java
+++ android/app/src/main/java/com/covid19tracker/MainActivity.java
@@ -1,6 +1,8 @@
 package com.covid19tracker;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +13,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "covid19tracker";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this, R.style.SplashScreenTheme);  // here
+    super.onCreate(savedInstanceState);
   }
 }
